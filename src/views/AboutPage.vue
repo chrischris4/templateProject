fb171a49eb287f0603688676769a35bfea7df151
christopher jost <jostchristopher44@gmail.com>
2025-04-24 21:50:53 +0200

    add vue router

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,19 @@
+<!-- src/views/AboutPage.vue -->
+<template>
+    <div>
+      <h1>About Page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage',
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #e74c3c;
+  }
+  </style>
+  
---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #3498db;
+  }
+  </style>
+  
